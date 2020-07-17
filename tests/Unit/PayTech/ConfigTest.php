b072f54af24034bebd37be7fe7a835d71395b173
PapiHack <mssmbaye@gmail.com>
2020-07-17 20:40:42 +0000

    set env to prod by default & start writting some test for Config class

--- tests/Unit/PayTech/ConfigTest.php
+++ tests/Unit/PayTech/ConfigTest.php
@@ -2,9 +2,33 @@
 
 namespace Tests\Unit\PayTech;
 
+use \PayTech\Config;
+
 /**
  * 
  * @author PapiHack
  * @since 07/2020
  * 
  */
+
+ it('Should have XOF currency by default', function() {
+     $this->assertSame(\PayTech\Enums\Currency::XOF, strtoupper(Config::getCurrency()));
+ });
+
+ test('LiveMode should be activate by default', function () {
+     $this->assertTrue(Config::getLiveMode());
+ });
+
+ test('TestMode should be disable by default', function () {
+     $this->assertFalse(Config::getTestMode());
+ });
+
+ test('Mobile config should be disable by default', function () {
+     $this->assertFalse(Config::getIsMobile());
+ });
+
+ test('Notification Urls should be empty by default', function () {
+     $this->assertEmpty(Config::getIpnUrl());
+     $this->assertEmpty(Config::getSuccessUrl());
+     $this->assertEmpty(Config::getCancelUrl());
+ });
--- tests/Unit/Utils/MakeRequestTest.php
+++ tests/Unit/Utils/MakeRequestTest.php
@@ -11,14 +11,37 @@ use \PayTech\Utils\MakeRequest;
  * 
  */
 
- it('', function () {
-
+ it('Should have headers & timeout static attributes', function () {
+     $this->assertClassHasStaticAttribute('headers', MakeRequest::class);
+     $this->assertClassHasStaticAttribute('timeout', MakeRequest::class);
  });
 
- it('', function () {
-
+ test('Timeout attribute should be 15 by default', function () {
+     $this->assertEquals(15, MakeRequest::getTimeout());
 });
 
-it('', function () {
+test('Headers attribute should be empty by default', function () {
+    $this->assertEmpty(MakeRequest::getHeaders());
+});
 
+it('Should change default timeout value & set headers attrivute', function () {
+    MakeRequest::setTimeout(20);
+    MakeRequest::setHeaders(['ACCEPT' => 'application/json;charset=utf-8']);
+    $this->assertNotEmpty(MakeRequest::getHeaders());
+    $this->assertGreaterThanOrEqual(15, MakeRequest::getTimeout());
+});
+
+it('Should make a post request with json data', function () {
+    $response = MakeRequest::json('http://papihack/dashboard/', ['name' => 'papi']);
+    $this->assertEmpty($response);
+});
+
+it('Should make a post request', function () {
+    $response = MakeRequest::post('http://papihack/dashboard/', ['name' => 'papi']);
+    $this->assertEmpty($response);
+});
+
+it('Should make a get request', function () {
+    $response = MakeRequest::get('http://papihack/dashboard/');
+    $this->assertEmpty($response);
 });
